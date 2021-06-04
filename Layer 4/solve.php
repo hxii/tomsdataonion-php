3abc0e5ce8f0b1acb0ba2655798604562032743b
Paul Glushak <paul@Pauls-MacBook-Pro.local>
2021-06-04 08:44:35 +0300

    Update

--- Layer 4/solve.php
+++ Layer 4/solve.php
@@ -9,26 +9,49 @@ $packets = getPackets($raw);
 filterPackets($packets);
 foreach ($packets as $packet) {
 	print_r($packet);
+	// print($packet['data']);
 }
 
 function str2hex(string $string) {
 	$return = '';
 	for ($i = 0; $i < strlen($string); $i++) {
-		$hex     = sprintf("%X", mb_ord($string[$i])); // Character => decimal value => hexadecimal value
-		$return .= str_pad($hex, 2, '0', STR_PAD_LEFT); // Pad with leading zero if shorter than two
+		// $hex     = sprintf("%x", mb_ord($string[$i])); // Character => decimal value => hexadecimal value
+		// var_dump($string[$i]);
+		// var_dump(mb_ord($string[$i]));
+		// var_dump(ord($string[$i]));
+		// var_dump(dechex(ord($string[$i])));
+		$hex     = unpack("H*", $string[$i]);
+		// var_dump($hex);
+		// die();
+		$return .= str_pad($hex[1], 2, '0', STR_PAD_LEFT); // Pad with leading zero if shorter than two
 	}
 	return $return;
 }
 
-function padbin(string $binary) : String {
-	return (strlen($binary) < 16) ? str_pad($binary, 16, '0', STR_PAD_LEFT) : $binary;
+function str2bin(string $string) {
+	$return = '';
+	for ($i = 0; $i < strlen($string); $i++) {
+		$bin = unpack('I*', $string[$i]);
+		$return .= padbin($bin);
+	}
+}
+
+function padbin(string $binary, $padding = 16) : String {
+	return (strlen($binary) < $padding) ? str_pad($binary, $padding, '0', STR_PAD_LEFT) : $binary;
+}
+
+function padhex($hex, int $padding = 2) : String {
+	if (strlen($hex) < $padding) {
+		$hex = str_pad($hex, $padding, '0', STR_PAD_LEFT);
+	}
+	return $hex;
 }
 
 function parseUDPHeader(string $header) : Array {
 	return [
-		'source_port'      => base_convert(substr($header, 0, 4), 16, 10),
-		'destination_port' => base_convert(substr($header, 4, 4), 16, 10),
-		'length'           => base_convert(substr($header, 8, 4), 16, 10),
+		'source_port'      => substr($header, 0, 4)
+		'destination_port' => substr($header, 4, 4),
+		'length'           => substr($header, 8, 4),
 		'checksum'         => substr($header, 12, 4),
 	];
 }
@@ -40,14 +63,14 @@ function parseIPV4Header(string $header) : Array {
 		'version'        => substr($header, 0, 1), // 4
 		'header_length'  => substr($header, 1, 1), // 5
 		'type'           => substr($header, 2, 2), // 00
-		'total_length'   => base_convert(substr($header, 4, 4), 16, 10), // 00 44
+		'total_length'   => substr($header, 4, 4), // 00 44
 		'identification' => substr($header, 8, 4), // ad 0b
 		'flags'          => substr($header, 12, 4), // 00 00
 		'ttl'            => substr($header, 16, 2), // 40
 		'protocol'       => substr($header, 18, 2), // 11
 		'checksum'       => substr($header, 20, 4), // 72 72
-		'source'         => hex2IP(substr($header, 24, 8)), // ac 14 02 fd
-		'destination'    => hex2IP(substr($header, 32, 8)), // ac 14 02 fd
+		'source'         => substr($header, 24, 8), // ac 14 02 fd
+		'destination'    => substr($header, 32, 8), // ac 14 02 fd
 	];
 }
 
@@ -57,7 +80,9 @@ function pseudoHeader(array $packet) {
 		'destination' => $packet['ipv4_header']['destination'],
 		'zeroes' => '00',
 		'protocol' => $packet['ipv4_header']['protocol'],
-		'length' => ''
+		'udp_length' => $packet['udp_header']['length'],
+		'source_port' => $packet['udp_header']['source_port'],
+		'destination_port' => $packet['udp_header']['destination_port'],
 	];
 }
 
@@ -75,8 +100,9 @@ function getPacket(&$payload) {
 	if ($ipv4_header && $udp_header) { // If we were able to get the substring
 		$header  = parseIPV4Header(str2hex($ipv4_header)); // Parse IPv4 header
 		$udp     = parseUDPHeader(str2hex($udp_header)); // Parse UDP header
-		$data    = substr($payload, 28, $header['total_length'] - 28); // Get data based on total_length - header length
-		$payload = substr($payload, $header['total_length']); // Remove packet from payload
+		$length  = base_convert($header['total_length'], 16, 10); // Convert length to decimal
+		$data    = substr($payload, 28, $length - 28); // Get data based on total_length - header length
+		$payload = substr($payload, $length); // Remove packet from payload
 		return [ // Return packet
 			'ipv4_header' => $header,
 			'udp_header'  => $udp,
@@ -96,6 +122,7 @@ function getPackets(string $payload) : Array {
 
 function filterPackets(array &$packets) {
 	$packets = array_filter($packets, function($item) {
-		return $item['ipv4_header']['source'] == '10.1.1.10';
+		return (hex2IP($item['ipv4_header']['source']) == '10.1.1.10')
+		&& (hex2IP($item['ipv4_header']['destination']) == '10.1.1.200');
 	});
 }
